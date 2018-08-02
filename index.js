e76df6ebbc0caa1e8459d0f1600e52e30085cf13
haileebn <hainvyp@gmail.com>
2018-08-02 15:32:54 +0700

    ...

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function fakeRecordOneKit() {
         {
           name: 'PMS5003',
           Time: new Date().getTime(),
-          Data: [getRandomInt(40, 60), getRandomInt(60, 80), getRandomInt(80, 120)]
+          Data: [getRandomInt(5, 10), getRandomInt(10, 40), getRandomInt(40, 60)]
         },
       ]
     };
